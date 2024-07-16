7e3321e6db0a86f0724e3602038665672aa1e340
sosoeeee <809953371@qq.com>
2024-07-16 16:35:35 +0800

    add bt plugins: predict goal client

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/predict_goal_action.hpp
@@ -0,0 +1,70 @@
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__PREDICT_GOAL_ACTION_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__PREDICT_GOAL_ACTION_HPP_
+
+#include <string>
+
+#include "nav2_msgs/action/predict_goal.hpp"
+#include "nav2_behavior_tree/bt_action_node.hpp"
+
+namespace nav2_behavior_tree
+{
+
+/**
+ * @brief A nav2_behavior_tree::BtActionNode class that wraps nav2_msgs::action::PredictGoal
+ */
+class PredictGoalAction : public BtActionNode<nav2_msgs::action::PredictGoal>
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::PredictGoalAction
+   * @param xml_tag_name Name for the XML tag for this node
+   * @param action_name Action name this node creates a client for
+   * @param conf BT node configuration
+   */
+  PredictGoalAction(
+    const std::string & xml_tag_name,
+    const std::string & action_name,
+    const BT::NodeConfiguration & conf);
+
+  /**
+   * @brief Function to perform some user-defined operation on tick
+   */
+  void on_tick() override;
+
+  /**
+   * @brief Function to perform some user-defined operation upon successful completion of the action
+   */
+  BT::NodeStatus on_success() override;
+
+  /**
+   * @brief Function to perform some user-defined operation upon abortion of the action
+   */
+  BT::NodeStatus on_aborted() override;
+
+  /**
+   * @brief Function to perform some user-defined operation upon cancelation of the action
+   */
+  BT::NodeStatus on_cancelled() override;
+
+  /**
+   * \brief Override required by the a BT action. Cancel the action and set the path output
+   */
+  void halt() override;
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing basic ports along with node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return providedBasicPorts(
+      {
+        BT::OutputPort<geometry_msgs::msg::PoseStamped>("predicted_goal", "Goal predited by RL agent"),
+        BT::InputPort<geometry_msgs::msg::PoseStamped>("goal", "Global destination"),
+      });
+  }
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__PREDICT_GOAL_ACTION_HPP_
---
+++ nav2_behavior_tree/plugins/action/predict_goal_action.cpp
@@ -0,0 +1,64 @@
+#include <memory>
+#include <string>
+
+#include "nav2_behavior_tree/plugins/action/predict_goal_action.hpp"
+
+namespace nav2_behavior_tree
+{
+
+PredictGoalAction::PredictGoalAction(
+  const std::string & xml_tag_name,
+  const std::string & action_name,
+  const BT::NodeConfiguration & conf)
+: BtActionNode<nav2_msgs::action::PredictGoal>(xml_tag_name, action_name, conf)
+{
+}
+
+void PredictGoalAction::on_tick()
+{
+  getInput("goal", goal_.goal);
+}
+
+BT::NodeStatus PredictGoalAction::on_success()
+{
+  setOutput("predicted_goal", result_.result->predicted_goal);
+  return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus PredictGoalAction::on_aborted()
+{
+  // when goal is aborted, the predicted goal will be global goal
+  setOutput("predicted_goal", result_.result->predicted_goal);
+  return BT::NodeStatus::FAILURE;
+}
+
+BT::NodeStatus PredictGoalAction::on_cancelled()
+{
+  // when goal is cancelled, the predicted goal will be global goal
+  setOutput("predicted_goal", result_.result->predicted_goal);
+  return BT::NodeStatus::SUCCESS;
+}
+
+void PredictGoalAction::halt()
+{
+  geometry_msgs::msg::PoseStamped predicted_goal;
+  getInput("goal", predicted_goal);
+  setOutput("predicted_goal", predicted_goal);
+  BtActionNode::halt();
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  BT::NodeBuilder builder =
+    [](const std::string & name, const BT::NodeConfiguration & config)
+    {
+      return std::make_unique<nav2_behavior_tree::PredictGoalAction>(
+        name, "predict_goal", config);
+    };
+
+  factory.registerBuilder<nav2_behavior_tree::PredictGoalAction>(
+    "PredictGoal", builder);
+}
