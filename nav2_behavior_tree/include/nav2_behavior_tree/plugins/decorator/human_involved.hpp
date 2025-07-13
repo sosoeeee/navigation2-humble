a83a517e4b634df3e15084f49f9d689fe10e251a
sosoeeee <809953371@qq.com>
2025-07-13 12:16:58 +0800

    implement BT decorator, which ticks its child only when human involving.

---
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/human_involved.hpp
@@ -0,0 +1,67 @@
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__HUMAN_INVOLVED_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__HUMAN_INVOLVED_HPP_
+
+#include <memory>
+#include <string>
+
+#include "std_msgs/msg/string.hpp"
+
+#include "behaviortree_cpp_v3/decorator_node.h"
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav2_behavior_tree
+{
+
+/**
+ * @brief A BT::DecoratorNode that subscribes to a topic of the length of the human involved path. It 
+ * ticks its child node only if the human involved path is greater than human_involved_path_length.
+ */
+class HumanInvolved : public BT::DecoratorNode
+{
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::HumanInvolved
+   * @param xml_tag_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  HumanInvolved(
+    const std::string & xml_tag_name,
+    const BT::NodeConfiguration & conf);
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<int>("human_involved_path_length", 5, "Length of the human involved path to trigger the child node")
+    };
+  }
+
+private:
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  /**
+   * @brief Callback function for goal update topic
+   * @param msg Shared pointer to std_msgs::msg::String message
+   */
+  void callback_human_involved(const std_msgs::msg::String::SharedPtr msg);
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr human_involved_sub_;
+  bool human_involving_ = false;
+  int human_involved_path_length_ = 0;
+
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::CallbackGroup::SharedPtr callback_group_;
+  rclcpp::executors::SingleThreadedExecutor callback_group_executor_;
+};
+
+}  // namespace nav2_behavior_tree
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__HUMAN_INVOLVED_HPP_
---
+++ nav2_behavior_tree/plugins/decorator/human_involved.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <memory>
+
+#include "std_msgs/msg/string.hpp"
+#include "behaviortree_cpp_v3/decorator_node.h"
+
+#include "nav2_behavior_tree/plugins/decorator/human_involved.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace nav2_behavior_tree
+{
+
+using std::placeholders::_1;
+
+HumanInvolved::HumanInvolved(
+  const std::string & name,
+  const BT::NodeConfiguration & conf)
+: BT::DecoratorNode(name, conf)
+{
+  getInput("human_involved_path_length", human_involved_path_length_);
+
+  node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+  callback_group_ = node_->create_callback_group(
+    rclcpp::CallbackGroupType::MutuallyExclusive,
+    false);
+  callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+
+  rclcpp::SubscriptionOptions sub_option;
+  sub_option.callback_group = callback_group_;
+  human_involved_sub_ = node_->create_subscription<std_msgs::msg::String>(
+    "human_involved_path",
+    rclcpp::SystemDefaultsQoS(),
+    std::bind(&HumanInvolved::callback_human_involved, this, _1),
+    sub_option);
+}
+
+inline BT::NodeStatus HumanInvolved::tick()
+{
+  callback_group_executor_.spin_some();
+
+  if (human_involving_ || child_node_->executeTick() == BT::NodeStatus::RUNNING) {
+    
+    const BT::NodeStatus child_state = child_node_->executeTick();
+    switch (child_state) {
+      case BT::NodeStatus::RUNNING:
+        return BT::NodeStatus::RUNNING;
+
+      case BT::NodeStatus::SUCCESS:
+        return BT::NodeStatus::SUCCESS;
+
+      case BT::NodeStatus::FAILURE:
+      default:
+        return BT::NodeStatus::FAILURE;
+    }
+  }
+  else{
+    return BT::NodeStatus::SUCCESS;
+  }
+}
+
+void
+HumanInvolved::callback_human_involved(const std_msgs::msg::String::SharedPtr msg)
+{
+   if (static_cast<int>(msg->data) > human_involved_path_length_) {
+     human_involving_ = true;
+   }
+   else {
+     human_involving_ = false;
+   }
+}
+
+}  // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<nav2_behavior_tree::HumanInvolved>("HumanInvolved");
+}
